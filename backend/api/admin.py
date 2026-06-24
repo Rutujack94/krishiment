8b13670fef28e78068754039be9d7292dad3187d
Rutuja Khande <159935258+Rutujack94@users.noreply.github.com>
2026-06-24 18:44:51 +0530

    Refactor admin.py to simplify user model registration

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,87 +1,47 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import (
-    CustomUser,
-    Equipment,
-    Todo,
-    Inquiry,
-    Job,
-    JobApplication,
-    LabourRating,
-    LabourSkill,
-    LabourEarning,
-    Notification,
-)
+from .models import CustomUser, Todo, Landmark, LandmarkDistance
 
+# Register your models here.
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = (
-        'email',
-        'username',
-        'first_name',
-        'phone',
-        'role',
-        'is_staff',
-        'is_active'
-    )
+    list_display = ('email', 'username', 'first_name', 'phone', 'role', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active', 'role')
-    ordering = ('email',)
-    search_fields = ('email', 'username', 'first_name')
-
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {
-            'fields': (
-                'username',
-                'first_name',
-                'phone',
-                'role',
-                'address',
-                'latitude',
-                'longitude',
-                'is_available',
-            )
-        }),
-        ('Permissions', {
-            'fields': (
-                'is_staff',
-                'is_active',
-                'is_superuser',
-                'groups',
-                'user_permissions',
-            )
-        }),
-        ('Important dates', {
-            'fields': ('last_login', 'date_joined')
-        }),
+        ('Personal info', {'fields': ('username', 'first_name', 'phone', 'role')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
-
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': (
-                'email',
-                'username',
-                'first_name',
-                'phone',
-                'role',
-                'password1',
-                'password2',
-                'is_staff',
-                'is_active',
-            ),
-        }),
+            'fields': ('email', 'username', 'first_name', 'phone', 'role', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
     )
-
+    search_fields = ('email', 'username', 'first_name')
+    ordering = ('email',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(Equipment)
 admin.site.register(Todo)
-admin.site.register(Inquiry)
-admin.site.register(Job)
-admin.site.register(JobApplication)
-admin.site.register(LabourRating)
-admin.site.register(LabourSkill)
-admin.site.register(LabourEarning)
-admin.site.register(Notification)
+
+
+class LandmarkDistanceInline(admin.TabularInline):
+    model = LandmarkDistance
+    fk_name = 'from_landmark'
+    extra = 0
+
+
+@admin.register(Landmark)
+class LandmarkAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location_type', 'latitude', 'longitude')
+    list_filter = ('location_type',)
+    search_fields = ('name', 'address')
+    inlines = [LandmarkDistanceInline]
+
+
+@admin.register(LandmarkDistance)
+class LandmarkDistanceAdmin(admin.ModelAdmin):
+    list_display = ('from_landmark', 'to_landmark', 'distance_km', 'travel_time_min')
+    list_filter = ('from_landmark', 'to_landmark')
